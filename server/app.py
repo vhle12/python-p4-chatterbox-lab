f4e0b4ab36319454a05382ed47037c7cee741c9b
vhle12 <158373104+vhle12@users.noreply.github.com>
2024-06-26 14:53:22 -0400

    complete

--- server/app.py
+++ server/app.py
@@ -14,13 +14,50 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    
+    getter = [message.to_dict() for message in Message.query.order_by(Message.created_at).all()]
+    
+    if request.method == 'GET':
+        return getter, 200
+    
+    elif request.method == 'POST':
+        data = request.get_json()
+        
+        new_message = Message(
+            body = data.get('body'),
+            username = data.get('username'),
+            created_at = data.get('created_at'),
+            updated_at = data.get('updated_at')
+        )
+        
+        db.session.add(new_message)
+        db.session.commit()
+        
+        return new_message.to_dict(), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    
+    if not message:
+        return {'error': 'message not found'}, 404
+    
+    if request.method == 'PATCH':
+        data = request.get_json()
+        
+        message.body = data.get('body')
+        
+        db.session.add(message)
+        db.session.commit()
+        
+        return message.to_dict(), 200
+    
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        return {}, 204
 
 if __name__ == '__main__':
     app.run(port=5555)
